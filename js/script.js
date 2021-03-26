d7d47447b7abfcd90ad48c46f9f6593a3c4cc546
João Vitor <81437413+JoaoOliveira0605@users.noreply.github.com>
2021-03-26 17:33:50 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var valorEmDolarTexto = prompt ("Qual valor em dolar você quer converter?")
 
-alert (EU LHE AMO EDNAAA)
+alert ("EU LHE AMO EDNAAA")
 
 var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
 
