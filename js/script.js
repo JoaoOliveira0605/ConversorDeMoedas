44901101930253ce59127aab833f90c6e3a7ffa0
João Vitor <81437413+JoaoOliveira0605@users.noreply.github.com>
2021-03-27 16:42:50 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var valorEmDolarTexto = prompt ("Qual valor em dolar você quer converter?")
 
-
 var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
 
 var valorEmReal = valorEmDolarNumero * 5.69
 
 var valorEmRealDecimal = valorEmReal.toFixed(2)
 
-alert(valorEmRealDecimal)
+document.write ("<h2>" + valorEmDolarNumero + " U$ = " +valorEmRealDecimal + " R$" + "</h2>")
+ 
